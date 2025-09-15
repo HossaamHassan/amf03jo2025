7557e09a583b35a2d891d669b58037b9b33e2e44
HossaamHassan <hossam.h.eldeeb@gmail.com>
2025-09-15 09:48:54 +0300

    Modified Some Files

--- js/main.js
+++ js/main.js
@@ -1,62 +1,21 @@
 $(document).ready(function () {
-    // Cached Selectors
-    const $window = $(window);
-    const $menuButton = $("#menu-button");
-    const $searchContainer = $(".main-navigation-search-container");
-    const $cssMenu = $("#cssmenu");
-    const $body = $("body");
-
-    // Check if an element is overflowing
-    function checkIfOverflowing($element) {
-        const elementOffset = $element.offset().left;
-        const elementWidth = $element.outerWidth();
-        const viewportWidth = $window.width();
-        const isArabic = $body.hasClass("arabic-version");
-
-        const isOverflowing = isArabic
-            ? elementOffset < 0 || elementOffset + elementWidth > viewportWidth
-            : elementOffset + elementWidth > viewportWidth || elementOffset < 0;
-
-        $element.toggleClass("overflowing-menu-container", isOverflowing);
-    }
-
-    // Overflow handling for submenu
-    $("li.has-sub")
-        .on("mouseenter touchstart", function () {
-            checkIfOverflowing($(this).find("> ul"));
-        })
-        .on("mouseleave touchend", function () {
-            $(this).find("> ul").removeClass("overflowing-menu-container");
-        });
-
-    // Initialize the menu
-    $cssMenu.menumaker({ title: "", format: "multitoggle" });
-
-    // Search bar toggle
-    $searchContainer.on("click", ".navigation-search", () => {
-        $(".searchBarOpen").addClass("search-active");
-    });
-    $(".searchBarOpen--closeBtn").click(() => {
-        $(".searchBarOpen").removeClass("search-active");
-    });
-
-    // Dropdown handling for sign-in button
-    $(".dropdown.signin-btn > .dropdown-toggle").click(() => {
-        $menuButton.removeClass("menu-opened");
-        $cssMenu.find("ul, .cssmenu2 ul").removeClass("open").css("display", "");
-    });
-    // Footer
-
+    // Navbar and Footer
     $(this).find(".fa-plus").show();
     $(this).find(".fa-minus").hide();
-
-    $(".toggleButton").click(function () {
-        // Check if the window width is less than or equal to 991px
+    $(".toggleButton, .toggleButtonHeader").click(function () {
         if ($(window).width() <= 991) {
-            // Toggle the visibility of the target element using classes
-            $(this).closest("div").next("ul").toggleClass("show");
-            // Toggle the icon based on the presence of the 'show' class
-            if ($(this).closest(".footer-item").find("ul").hasClass("show")) {
+            let dropdown;
+
+            if ($(this).closest(".footer-item").length) {
+                dropdown = $(this).closest("div").next("ul");
+            }
+
+            if ($(this).closest("li").find(".dropdown-menu-news").length) {
+                dropdown = $(this).closest("li").find(".dropdown-menu-news");
+            }
+            dropdown.toggleClass("show");
+
+            if (dropdown.hasClass("show")) {
                 $(this).find(".fa-plus").hide();
                 $(this).find(".fa-minus").show();
             } else {
@@ -66,36 +25,14 @@ $(document).ready(function () {
         }
     });
 
-    // Back to top button
-    $(".backtotop-box").click(() => {
-        $("html, body").animate({ scrollTop: 0 }, "fast");
+    // Search
+    $(".search-btn").on("click", function (e) {
+        e.preventDefault();
+        $(".searchBarOpen").addClass("active");
     });
 
-    // Navbar active link toggle
-    $(".nav-link").click(function () {
-        $(".nav-link").removeClass("active");
-        $(this).addClass("active");
-    });
-
-    // Navbar search toggle
-    $(".search-icon").click(() => {
-        $(".nav-search, .navbar-search-bar-container").addClass("active");
-    });
-    $(".close-icon").click(() => {
-        $(".nav-search, .navbar-search-bar-container").removeClass("active");
-    });
-
-    // Submenu toggle in the navigation
-    $(".has-sub").click(function () {
-        const $submenu = $(this).find("> ul");
-        const isOpen = $submenu.hasClass("open");
-
-        $("li.has-sub > ul").removeClass("open").css("display", "");
-        $("li.has-sub > span.submenu-button").removeClass("submenu-opened");
-
-        if (!isOpen) {
-            $submenu.addClass("open").css("display", "block");
-            $(this).find("span.submenu-button").addClass("submenu-opened");
-        }
+    $(".searchBarOpen--closeBtn").on("click", function () {
+        $(".searchBarOpen").removeClass("active");
     });
 });
+
