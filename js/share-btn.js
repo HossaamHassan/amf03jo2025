365ff828e6d3a3f61995f9c5f89855a62f8a3aa2
Braa Refaat <braarefat2002@gmail.com>
2025-10-01 15:51:34 +0300

    update share button and update video gallary nav buttons

--- js/share-btn.js
+++ js/share-btn.js
@@ -1,18 +1,42 @@
 $(document).ready(function () {
-  const $document = $(document);
+  window.openShareOptions = function (button) {
+      const $shareButton = $(button);
+      const $card = $shareButton.closest('.media-card-buttons');
+      const $shareOptions = $card.find('.share-options');
+      const $mainBtn = $card.find('.main-btn');
+      const $videoBtn = $card.find('.media-video-button');
 
-  $document.on("click", ".share-btn", function () {
-    const $this = $(this);
-    const $overlay = $this.siblings(".share-overlay");
-    $overlay.addClass("active");
-    $this.hide();
-  });
+      // Hide share button itself
+      $shareButton.addClass("hidden");
 
-  $document.on("click", ".share-overlay #closeButton", function () {
-    const $this = $(this);
-    const $overlay = $this.closest(".share-overlay");
-    const $shareBtn = $overlay.siblings(".share-btn");
-    $overlay.removeClass("active");
-    $shareBtn.show();
-  });
+      // Show share options
+      $shareOptions.addClass("active");
+
+      // Hide read more
+      $mainBtn.addClass("hidden");
+
+      // Hide video button
+      $videoBtn.addClass("hidden");
+  };
+
+  window.closeShareOptions = function (button) {
+      const $closeButton = $(button);
+      const $card = $closeButton.closest('.media-card-buttons');
+      const $shareOptions = $card.find('.share-options');
+      const $mainBtn = $card.find('.main-btn');
+      const $shareButton = $card.find('.media-share-button');
+      const $videoBtn = $card.find('.media-video-button');
+
+      // Show share button again
+      $shareButton.removeClass("hidden");
+
+      // Hide share options
+      $shareOptions.removeClass("active");
+
+      // Show read more
+      $mainBtn.removeClass("hidden");
+
+      // Show video button
+      $videoBtn.removeClass("hidden");
+  };
 });
